f08c8a64935872e3b6f93a0cf5b59059845cd69b
Chris Szeluga <chris.szeluga@yahoo.com>
2015-02-28 23:49:32 -0500

    begin events js

--- js/main.js
+++ js/main.js
@@ -1,8 +1,19 @@
-// FACEBOOK
+// HERO PARALLAX
 
+var hero = document.getElementsByClassName("hero")[0];
+
+window.onscroll = function() {
+    var scrolled = window.pageYOffset;
+    hero.style["background-position"] = "50% " + (scrolled / 4) + "px";
+}
+
+// FACEBOOK
+var loaded = false;
 window.fbAsyncInit = function() {
     FB.init({
         appId: '803530653059405',
+        status: true,
+        cookie: true,
         xfbml: true,
         version: 'v2.2'
     });
@@ -21,6 +32,7 @@ window.fbAsyncInit = function() {
 
 // Get facebook cover from google events description
 function getCover(gDesc) {
+    gDesc = '[https://www.facebook.com/events/432650743556252]This is a test description!';
     if (gDesc) {
 
         // Get facebook event ID
@@ -34,23 +46,28 @@ function getCover(gDesc) {
             // Try to get facebook event ID
             var eventID = '/' + matches[0].split('/')[4];
 
-            // FB.api(
-            //     'StartupShell/events',
-            //     function(response) {
-            //         console.log('res');
-            //         console.log(response);
-            //         if (response && !response.error) {
-            //             console.log('back');
-            //             console.log(response);
-            //         }
-            //     }
-            // );
+            FB.api(
+                eventID + '/picture',
+                function(response) {
+
+                    if (response && !response.error) {
+                        var cover = new Image();
+                        cover.src = response.data.url;
+                        return cover;
+                    } else {
+                        return '';
+                    }
+                }, {
+                    access_token: '803530653059405|GkX1fG84RU3rCKQL_epc_AT7ZaA',
+                    redirect: false,
+                    type: 'large'
+                }
+            );
 
         }
 
     }
 }
-
 // EVENTS
 
 // format time
@@ -73,12 +90,19 @@ function timeFormat(dateInput) {
 }
 
 // this is to template the data for the dom
-function assembleStructure(data) {
+function assembleStructure(data, index) {
     var d = new Date(data.start);
     var startString = timeFormat(d);
-    return ['<div class="event">',
+    if (index == 0) {
+        var featured = ' featured';
+    }
+    return ['<div class="event',
+        featured || '',
+        '">',
         '<div class="image">',
-        '<img src="http://placehold.it/900x500">',
+
+        data.cover,
+
         '</div>',
         '<div class="meta">',
         '<div class="title"><a href=', data.link, '>', data.title, '</a></div>',
@@ -136,12 +160,10 @@ request.onload = function() {
         })
 
         // append to DOM
-        .forEach(function(i) {
-            var eventStream = document.getElementById("google-events");
-            eventStream.innerHTML = eventStream.innerHTML + assembleStructure(i);
+        .forEach(function(i, index) {
+            var eventStream = document.getElementById("eventStream");
+            eventStream.innerHTML = eventStream.innerHTML + assembleStructure(i, index);
         });
-
-
 
     } else {
         // We reached our target server, but it returned an error
@@ -154,12 +176,3 @@ request.onerror = function() {
 };
 
 request.send();
-
-// Hero Parallax Effect
-
-var hero = document.getElementsByClassName("hero")[0];
-
-window.onscroll = function() {
-    var scrolled = window.pageYOffset;
-    hero.style["background-position"] = "50% " + (scrolled / 4) + "px";
-}
