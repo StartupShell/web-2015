89c208b1362106e710950e533de1289253be5c11
Luis Santos <samaisantos@gmail.com>
2015-03-02 00:54:53 -0500

    fixed snapping bug in parallax effect

--- js/main.js
+++ js/main.js
@@ -4,5 +4,5 @@ var hero = document.getElementsByClassName("hero")[0];
 
 window.onscroll = function() {
     var scrolled = window.pageYOffset;
-    hero.style["background-position"] = "50% " + (scrolled / 4) + "px";
+    hero.style["background-position"] = "50% " + (50 + (scrolled / 25)) + "%";
 }
