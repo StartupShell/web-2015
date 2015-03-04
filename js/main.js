d0ec004eba637a7c75aaab9abf1f714270a52c12
Chris Szeluga <chris.szeluga@yahoo.com>
2015-03-04 00:15:41 -0500

    create mobile menu

--- js/main.js
+++ js/main.js
@@ -1,6 +1,46 @@
 // HERO PARALLAX
 
-window.onscroll = function() {
-    var scrolled = window.pageYOffset;
-    $('.hero').style["background-position"] = "50% " + (50 + (scrolled / 25)) + "%";
+$(document).ready(function() {
+    $(window).scroll(function() {
+        var scrolled = window.pageYOffset;
+        $('.hero').style["background-position"] = "50% " + (50 + (scrolled / 25)) + "%";
+    })
+});
+
+// COLLAPSE MENU
+
+function mobileMenu() {
+    var width = $(window).width();
+
+    if (width < 960) {
+        // Wrap the nav & social in the .nav element
+        $('.menu ul').wrapAll("<div class='links' />");
+
+        // Hide the new element & show the hamburglar
+        // $('nav .links').hide();
+        $('nav .hamburglar').show();
+
+        $('nav .hamburglar').click(function() {
+            $('.menu').toggleClass('open');
+            $('body').toggleClass('open');
+        })
+
+    } else {
+    	// Unwrap if wrapped
+    	if($('.menu ul').parent().is('.links')) {
+    		$('.menu ul').unwrap();
+    	}
+
+    	$('nav .hamburglar').hide();
+    }
+
+
 }
+
+$(document).ready(function() {
+    mobileMenu();
+});
+
+$(window).resize(function() {
+    mobileMenu();
+});
