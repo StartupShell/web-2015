b95050435f7bee9d37569d72ad947dfd1df2e7d6
Chris Szeluga <chris.szeluga@yahoo.com>
2015-03-04 01:19:26 -0500

    Mailchimp form integration

--- js/main.js
+++ js/main.js
@@ -26,12 +26,12 @@ function mobileMenu() {
         })
 
     } else {
-    	// Unwrap if wrapped
-    	if($('.menu ul').parent().is('.links')) {
-    		$('.menu ul').unwrap();
-    	}
+        // Unwrap if wrapped
+        if ($('.menu ul').parent().is('.links')) {
+            $('.menu ul').unwrap();
+        }
 
-    	$('nav .hamburglar').hide();
+        $('nav .hamburglar').hide();
     }
 
 
@@ -44,3 +44,57 @@ $(document).ready(function() {
 $(window).resize(function() {
     mobileMenu();
 });
+
+// MAILCHIMP
+// -- Adapted from http://stackoverflow.com/questions/8425701/ajax-mailchimp-signup-form-integration
+
+$(document).ready(function() {
+
+    // Validate email
+    function isEmail(email) {
+        var regex = /^([a-zA-Z0-9_.+-])+\@(([a-zA-Z0-9-])+\.)+([a-zA-Z0-9]{2,4})+$/;
+        return regex.test(email);
+    }
+
+    if ($('.subscribe form').length > 0) {
+        $('form input[type="submit"]').bind('click', function(event) {
+            if (event) event.preventDefault();
+
+            if (!isEmail($(".subscribe form [name='email']").val())) {
+                alert('Email address is not valid.');
+            } else {
+                register($('.subscribe form'));
+            }
+        });
+    }
+});
+
+function register($form) {
+    // Prepare for Mailchimp
+    var email = $form.find('input[type=email]');
+    email.attr('name', 'EMAIL');
+
+    var data = {};
+    var dataArray = $form.serializeArray();
+    $.each(dataArray, function(index, item) {
+        data[item.name] = item.value;
+    });
+
+    $.ajax({
+        url: '//startupshell.us8.list-manage2.com/subscribe/post-json?u=ab309f640b0f94f8e5fd0a2e8&amp&id=af8824bb76&c=?',
+        data: data,
+        dataType: 'jsonp',
+        error: function(err) {
+            console.log(err);
+            alert("Could not connect to the subscription server. Please try again later.");
+        },
+        success: function(data) {
+            console.log(data);
+            if (data.result != "success") {
+                alert("Submittion error. Please try again.");
+            } else {
+                $('.subscribe form input[type=submit]').prop('value', 'Success!');
+            }
+        }
+    });
+}
