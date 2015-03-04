d498cb3661a551e2917ef3d27e767db3d5d3acc5
Chris Szeluga <chris.szeluga@yahoo.com>
2015-03-03 22:08:10 -0500

    add jquery

--- js/main.js
+++ js/main.js
@@ -6,3 +6,39 @@ window.onscroll = function() {
     var scrolled = window.pageYOffset;
     hero.style["background-position"] = "50% " + (50 + (scrolled / 25)) + "%";
 }
+
+
+// MENU
+
+function mobileMenu() {
+	// Detect current browser width
+	var screenWidth = document.body.clientWidth;
+
+	if(screenWidth <= 960) {
+
+		var menu = document.getElementsByClassName('menu')[0];
+
+		if menu.hasClass()
+
+		// Put social icons inside the menu
+
+		var social = document.getElementsByClassName('social-list')[0];
+		var menuList = document.getElementsByClassName('menu-list')[0];
+		menuList.innerHTML = '<div class="menu-list">' + menuList.innerHTML + '<div class="social-list">' + social.innerHTML + '</div></div>';
+
+		
+
+		// Remove top level social menu
+		menu.removeChild(menu.childNodes[1]);
+
+		menu.className = menu.className + 'collapsed';
+
+		// Collapse menu
+	}
+}
+
+window.onresize = function() {
+	mobileMenu()
+}
+
+mobileMenu();
