e39715450055aa9e8cbb62ec1a6d275f875b0f9d
jaxgeller <jacksongeller@gmail.com>
2015-03-05 16:52:26 -0500

    general refactor

--- js/events.js
+++ js/events.js
@@ -1,58 +1,58 @@
 // FACEBOOK
 function fbEnsureInit(callback) {
-    if (!window.fbApiInit) {
-        setTimeout(function() {
-            fbEnsureInit(callback);
-        }, 50);
-    } else {
-        if (callback) {
-            callback();
-        }
+  if (!window.fbApiInit) {
+    setTimeout(function() {
+      fbEnsureInit(callback);
+    }, 50);
+  } else {
+    if (callback) {
+      callback();
     }
+  }
 }
 window.fbAsyncInit = function() {
-    FB.init({
-        appId: '803530653059405',
-        status: true,
-        cookie: true,
-        xfbml: true,
-        version: 'v2.2'
-    });
+  FB.init({
+    appId: '803530653059405',
+    status: true,
+    cookie: true,
+    xfbml: true,
+    version: 'v2.2'
+  });
 
-    fbApiInit = true;
+  fbApiInit = true;
 };
 
 (function(d, s, id) {
-    var js, fjs = d.getElementsByTagName(s)[0];
-    if (d.getElementById(id)) {
-        return;
-    }
-    js = d.createElement(s);
-    js.id = id;
-    js.src = "//connect.facebook.net/en_US/sdk.js";
-    fjs.parentNode.insertBefore(js, fjs);
+  var js, fjs = d.getElementsByTagName(s)[0];
+  if (d.getElementById(id)) {
+    return;
+  }
+  js = d.createElement(s);
+  js.id = id;
+  js.src = "//connect.facebook.net/en_US/sdk.js";
+  fjs.parentNode.insertBefore(js, fjs);
 }(document, 'script', 'facebook-jssdk'));
 
 // Reformat data & get facebook cover from google events description
 function getCover(data, callback) {
 
-    gDesc = data.description;
-    if (gDesc) {
+  gDesc = data.description;
+  if (gDesc) {
 
         // Check for cover override
         var override = [];
         gDesc.replace(/\{(.*?)\}/g, function(g0, g1) {
-            override.push(g1);
+          override.push(g1);
         })
 
         if (override[0]) {
-            data.cover = override[0];
+          data.cover = override[0];
         }
 
         // Get facebook event ID
         var matches = [];
         gDesc.replace(/\[(.*?)\]/g, function(g0, g1) {
-            matches.push(g1);
+          matches.push(g1);
         })
 
         // Get the first tag
@@ -64,45 +64,45 @@ function getCover(data, callback) {
                 // Try to get facebook event ID
                 var eventID = '/' + matches[0].split('/')[4];
                 fbEnsureInit(function() {
-                    FB.api(
-                        eventID + '?fields=cover',
-                        function(response) {
-                            if (response && !response.error) {
-                                if (!data.cover) {
-                                    data.cover = response.cover.source;
-                                }
-                                data.link = 'https://www.facebook.com/events/' + response.id;
-                                callback(data);
-                            }
-                        }, {
-                            access_token: '803530653059405|GkX1fG84RU3rCKQL_epc_AT7ZaA',
-                            redirect: false,
-                            type: 'normal'
+                  FB.api(
+                    eventID + '?fields=cover',
+                    function(response) {
+                      if (response && !response.error) {
+                        if (!data.cover) {
+                          data.cover = response.cover.source;
                         }
+                        data.link = 'https://www.facebook.com/events/' + response.id;
+                        callback(data);
+                      }
+                    }, {
+                      access_token: '803530653059405|GkX1fG84RU3rCKQL_epc_AT7ZaA',
+                      redirect: false,
+                      type: 'normal'
+                    }
                     );
                 })
 
-            } else {
+              } else {
                 data.link = matches[0];
                 callback(data);
+              }
+
+
+            } else {
+              callback(data);
             }
 
-
-        } else {
+          } else {
             callback(data);
+          }
+
         }
-
-    } else {
-        callback(data);
-    }
-
-}
 
 // EVENTS
 
 // Format time
 function timeFormat(dateInput, noTime) {
-    var months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+  var months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
 
     // Return the formatted string
     if (noTime) {
@@ -111,21 +111,21 @@ function timeFormat(dateInput, noTime) {
         var date = new Date(timezoneOffset);
         date = [months[date.getMonth()], date.getDate()]
         return date.join(" ");
-    } else {
+      } else {
         var date = [months[dateInput.getMonth()], dateInput.getDate()];
         var time = [dateInput.getHours(), dateInput.getMinutes()];
         var suffix = (time[0] < 12) ? "AM" : "PM";
         time[0] = (time[0] < 12) ? time[0] : time[0] - 12;
         time[0] = time[0] || 12;
         for (var i = 1; i < 3; i++) {
-            if (time[i] < 10) {
-                time[i] = "0" + time[i];
-            }
+          if (time[i] < 10) {
+            time[i] = "0" + time[i];
+          }
         }
         return date.join(" ") + ", " + time.join(":") + " " + suffix;
-    }
+      }
 
-}
+    }
 
 // This is to template the data for the dom
 function assembleStructure(data, index, callback) {
@@ -135,94 +135,94 @@ function assembleStructure(data, index, callback) {
     if (!data.start.dateTime) {
         // All-day event
         newData.start = timeFormat(new Date(data.start.date), true);
-    } else {
+      } else {
         newData.start = timeFormat(new Date(data.start.dateTime));
-    }
-    newData.end = new Date(data.end.dateTime);
-    newData.description = data.description;
-    newData.title = data.summary;
-    newData.link = data.htmlLink;
-    newData.location = data.location;
-    newData.position = index;
+      }
+      newData.end = new Date(data.end.dateTime);
+      newData.description = data.description;
+      newData.title = data.summary;
+      newData.link = data.htmlLink;
+      newData.location = data.location;
+      newData.position = index;
 
     // Get the FB data for the event
     getCover(newData, function(fbData) {
         // If there's no cover image, use the default one
         if (!fbData.cover) {
-            var cover = '<div class="image no-fb"><img src="../assets/shell-logo-wire.svg"></div>';
+          var cover = '<div class="image no-fb"><img src="../assets/shell-logo-wire.svg"></div>';
         } else {
-            var cover = '<div class="image"><img src="' + fbData.cover + '"></div>';
+          var cover = '<div class="image"><img src="' + fbData.cover + '"></div>';
         }
 
         // Display gCal description for featured event
         var description;
         if (fbData.position == 0 && fbData.description) {
-            description = fbData.description;
+          description = fbData.description;
 
             // Strip the [] and {} tags.
             description = description.replace(/\[(.*?)\]/g, "");
             description = description.replace(/\{(.*?)\}/g, "");
-        }
+          }
 
         // Display gCal location if it has one
         var location;
         if (fbData.location) {
-            location = ' @ ' + fbData.location;
+          location = ' @ ' + fbData.location;
         }
 
         // Return this structure
         var ret = ['<a target="_blank" href="', fbData.link, '"><div class="details">',
-            cover,
-            '<div class="meta">',
-            '<h2 class="title">', fbData.title, '</h2>',
-            '<p class="date">', newData.start, location, '</p>',
-            '<p class="description">', description, '</p>',
-            '</div>',
-            '</div></a>'
+        cover,
+        '<div class="meta">',
+        '<h2 class="title">', fbData.title, '</h2>',
+        '<p class="date">', newData.start, location, '</p>',
+        '<p class="description">', description, '</p>',
+        '</div>',
+        '</div></a>'
         ].join('');
 
         callback(ret, fbData.position);
-    })
+      })
 
 }
 
 // This is where to get the data from
 var url = ['https://www.googleapis.com/calendar/v3/calendars',
-    '/7qvrobfs0js5799ebugodgc5go@group.calendar.google',
-    '.com/events?key=AIzaSyDd9bnLFkG8tyRgmjttiFRTT0MTtYpkZb8'
+'/7qvrobfs0js5799ebugodgc5go@group.calendar.google',
+'.com/events?key=AIzaSyDd9bnLFkG8tyRgmjttiFRTT0MTtYpkZb8'
 ].join('');
 
 
 $.ajax({
-    url: url
+  url: url
 }).done(function(data) {
         // Begin parsing
         data.items
 
         // Get rid of reoccuring
-            .filter(function(i) {
-            if (i.hasOwnProperty('start') && i.hasOwnProperty('end')) {
-                return true;
-            }
+        .filter(function(i) {
+          if (i.hasOwnProperty('start') && i.hasOwnProperty('end')) {
+            return true;
+          }
         })
 
         // Get the ones that havent happend yet
         .filter(function(i) {
-            if (i.end.dateTime) {
-                return new Date(i.end.dateTime) >= new Date().getTime();
-            } else {
+          if (i.end.dateTime) {
+            return new Date(i.end.dateTime) >= new Date().getTime();
+          } else {
                 // The event is all-day, so use the start time
                 // We add 86400000 (24 hours) so that the event still displays throughout the entire day
                 return new Date(i.end.date) >= new Date().getTime();
-            }
-        })
+              }
+            })
 
 
         // Sort by date
         .sort(function(a, b) {
-            var first = a.start.dateTime || a.start.date;
-            var second = b.start.dateTime || b.start.date;
-            return new Date(first) - new Date(second);
+          var first = a.start.dateTime || a.start.date;
+          var second = b.start.dateTime || b.start.date;
+          return new Date(first) - new Date(second);
         })
 
 
@@ -238,11 +238,11 @@ $.ajax({
 
                 // Feature first
                 if (position == 0) {
-                    $('.events .event:first-child').addClass('featured');
+                  $('.events .event:first-child').addClass('featured');
                 }
 
-            })
+              })
 
-        });
+          });
 
-});
+      });
--- js/main.js
+++ js/main.js
@@ -1,18 +1,18 @@
 // HERO PARALLAX
 
 $(document).ready(function() {
-    $(window).scroll(function() {
-        var scrolled = window.pageYOffset;
-        $('.hero').style["background-position"] = "50% " + (50 + (scrolled / 25)) + "%";
-    })
+  $(window).scroll(function() {
+    var scrolled = window.pageYOffset;
+    $('.hero').style["background-position"] = "50% " + (50 + (scrolled / 25)) + "%";
+  })
 });
 
 // COLLAPSE MENU
 
 $(document).ready(function() {
-    $('.slide-button').click(function() {
-        $('.menu').toggleClass('open');
-    })
+  $('.slide-button').click(function() {
+    $('.menu').toggleClass('open');
+  })
 });
 
 // MAILCHIMP
@@ -22,22 +22,22 @@ $(document).ready(function() {
 
     // Validate email
     function isEmail(email) {
-        var regex = /^([a-zA-Z0-9_.+-])+\@(([a-zA-Z0-9-])+\.)+([a-zA-Z0-9]{2,4})+$/;
-        return regex.test(email);
+      var regex = /^([a-zA-Z0-9_.+-])+\@(([a-zA-Z0-9-])+\.)+([a-zA-Z0-9]{2,4})+$/;
+      return regex.test(email);
     }
 
     if ($('.subscribe form').length > 0) {
-        $('form input[type="submit"]').bind('click', function(event) {
-            if (event) event.preventDefault();
+      $('form input[type="submit"]').bind('click', function(event) {
+        if (event) event.preventDefault();
 
-            if (!isEmail($(".subscribe form [name='email']").val())) {
-                alert('Email address is not valid.');
-            } else {
-                register($('.subscribe form'));
-            }
-        });
+        if (!isEmail($(".subscribe form [name='email']").val())) {
+          alert('Email address is not valid.');
+        } else {
+          register($('.subscribe form'));
+        }
+      });
     }
-});
+  });
 
 function register($form) {
     // Prepare for Mailchimp
@@ -47,22 +47,22 @@ function register($form) {
     var data = {};
     var dataArray = $form.serializeArray();
     $.each(dataArray, function(index, item) {
-        data[item.name] = item.value;
+      data[item.name] = item.value;
     });
 
     $.ajax({
-        url: '//startupshell.us8.list-manage2.com/subscribe/post-json?u=ab309f640b0f94f8e5fd0a2e8&amp&id=af8824bb76&c=?',
-        data: data,
-        dataType: 'jsonp',
-        error: function(err) {
-            alert("Could not connect to the subscription server. Please try again later.");
-        },
-        success: function(data) {
-            if (data.result != "success") {
-                alert("Submittion error. Please try again.");
-            } else {
-                $('.subscribe form input[type=submit]').prop('value', 'Success!');
-            }
+      url: '//startupshell.us8.list-manage2.com/subscribe/post-json?u=ab309f640b0f94f8e5fd0a2e8&amp&id=af8824bb76&c=?',
+      data: data,
+      dataType: 'jsonp',
+      error: function(err) {
+        alert("Could not connect to the subscription server. Please try again later.");
+      },
+      success: function(data) {
+        if (data.result != "success") {
+          alert("Submittion error. Please try again.");
+        } else {
+          $('.subscribe form input[type=submit]').prop('value', 'Success!');
         }
+      }
     });
-}
+  }
