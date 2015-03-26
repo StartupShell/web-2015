77f39b9b8f1f984d2e8634a25cdbcb0c4a206a33
Chris Szeluga <chris.szeluga@yahoo.com>
2015-03-26 07:52:58 -0400

    create demoday page

--- js/main.js
+++ js/main.js
@@ -1,18 +1,18 @@
 // HERO PARALLAX
 
-$(document).ready(function() {
-  $(window).scroll(function() {
-    var scrolled = window.pageYOffset;
-    $('.hero').css("background-position", "50% " + (60 + (scrolled / 25)) + "%");
-  })
-});
+// $(document).ready(function() {
+//     $(window).scroll(function() {
+//         var scrolled = window.pageYOffset;
+//         $('.hero').css("background-position", "50% " + (60 + (scrolled / 25)) + "%");
+//     })
+// });
 
 // COLLAPSE MENU
 
 $(document).ready(function() {
-  $('.slide-button').click(function() {
-    $('.menu').toggleClass('open');
-  })
+    $('.slide-button').click(function() {
+        $('.menu').toggleClass('open');
+    })
 });
 
 // MAILCHIMP
@@ -22,22 +22,22 @@ $(document).ready(function() {
 
     // Validate email
     function isEmail(email) {
-      var regex = /^([a-zA-Z0-9_.+-])+\@(([a-zA-Z0-9-])+\.)+([a-zA-Z0-9]{2,4})+$/;
-      return regex.test(email);
+        var regex = /^([a-zA-Z0-9_.+-])+\@(([a-zA-Z0-9-])+\.)+([a-zA-Z0-9]{2,4})+$/;
+        return regex.test(email);
     }
 
     if ($('.subscribe form').length > 0) {
-      $('form input[type="submit"]').bind('click', function(event) {
-        if (event) event.preventDefault();
+        $('form input[type="submit"]').bind('click', function(event) {
+            if (event) event.preventDefault();
 
-        if (!isEmail($(".subscribe form [name='email']").val())) {
-          alert('Email address is not valid.');
-        } else {
-          register($('.subscribe form'));
-        }
-      });
+            if (!isEmail($(".subscribe form [name='email']").val())) {
+                alert('Email address is not valid.');
+            } else {
+                register($('.subscribe form'));
+            }
+        });
     }
-  });
+});
 
 function register($form) {
     // Prepare for Mailchimp
@@ -47,22 +47,41 @@ function register($form) {
     var data = {};
     var dataArray = $form.serializeArray();
     $.each(dataArray, function(index, item) {
-      data[item.name] = item.value;
+        data[item.name] = item.value;
     });
 
     $.ajax({
-      url: '//startupshell.us8.list-manage2.com/subscribe/post-json?u=ab309f640b0f94f8e5fd0a2e8&amp&id=af8824bb76&c=?',
-      data: data,
-      dataType: 'jsonp',
-      error: function(err) {
-        alert("Could not connect to the subscription server. Please try again later.");
-      },
-      success: function(data) {
-        if (data.result != "success") {
-          alert("Submittion error. Please try again.");
-        } else {
-          $('.subscribe form input[type=submit]').prop('value', 'Success!');
+        url: '//startupshell.us8.list-manage2.com/subscribe/post-json?u=ab309f640b0f94f8e5fd0a2e8&amp&id=af8824bb76&c=?',
+        data: data,
+        dataType: 'jsonp',
+        error: function(err) {
+            alert("Could not connect to the subscription server. Please try again later.");
+        },
+        success: function(data) {
+            if (data.result != "success") {
+                alert("Submittion error. Please try again.");
+            } else {
+                $('.subscribe form input[type=submit]').prop('value', 'Success!');
+            }
         }
-      }
     });
-  }
+}
+
+
+// ADD TO GOOGLE CALENDAR
+(function() {
+    if (window.addtocalendar)
+        if (typeof window.addtocalendar.start == "function") return;
+    if (window.ifaddtocalendar == undefined) {
+        window.ifaddtocalendar = 1;
+        var d = document,
+            s = d.createElement('script'),
+            g = 'getElementsByTagName';
+        s.type = 'text/javascript';
+        s.charset = 'UTF-8';
+        s.async = true;
+        s.src = ('https:' == window.location.protocol ? 'https' : 'http') + '://addtocalendar.com/atc/1.5/atc.min.js';
+        var h = d[g]('body')[0];
+        h.appendChild(s);
+    }
+})();
